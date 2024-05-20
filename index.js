b66bb68ae64ea62a181029c887815a7d135f490a
John-Enes <joaoenesoficial@gmail.com>
2024-05-20 19:09:02 -0300

    FirstFinalAtt

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("btn-topo").style.display = "block";
-    } else {
-        document.getElementById("btn-topo").style.display = "none";
-    }
-}
-
-document.getElementById("btn-topo").addEventListener("click", function() {
-    document.body.scrollTop = 0; // Para navegadores Safari
-    document.documentElement.scrollTop = 0; // Para outros navegadores
-});
-
 ScrollReveal().reveal('.headline');
-ScrollReveal().reveal('.tagline', { delay: 5000});
+ScrollReveal().reveal('.tagline', { delay: 5000 });
 ScrollReveal().reveal('.punchline', { delay: 5000 });
+
+
