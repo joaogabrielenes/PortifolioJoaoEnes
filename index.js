1831c4fc8b522132f32ffb3eb10f6121e010beb7
John-Enes <joaoenesoficial@gmail.com>
2024-09-05 20:34:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,4 @@ ScrollReveal().reveal('.headline');
 ScrollReveal().reveal('.tagline', { delay: 5000 });
 ScrollReveal().reveal('.punchline', { delay: 5000 });
 
-$('.carousel').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
-  });
+
